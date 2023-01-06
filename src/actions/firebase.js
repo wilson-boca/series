fad179634cc668ffc9db09de4a044cd8b10e9941
wilson <wilson.boca@gmail.com>
2023-01-06 20:55:33 -0300

    Removing unused const

--- src/actions/firebase.js
+++ src/actions/firebase.js
@@ -1,13 +1,7 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyDXFAe_e23E8K8CQHFo3SbMV7k2bqQ6yHw',
-    authDomain: 'series-be2022.firebaseapp.com',
-    databaseURL: 'https://series-be2022-default-rtdb.firebaseio.com/',
-    projectId: 'series-be2022',
-    storageBucket: 'series-be2022.appspot.com',
-    messagingSenderId: '92054467150',
-    appId: '1:92054467150:web:2354fcc591f409f214091f'
+// paste credentials here
 };
 
 const app = initializeApp(firebaseConfig);
